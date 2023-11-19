24912932e88afb3c4d16fe6b2723321af4113c70
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-11-20 00:08:45 +0530

    refactor: General AppDelegate cleanup

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -11,12 +11,12 @@ import UIKit
 let appColor : UIColor = .systemTeal
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     let loginViewController = LoginViewController()
     let onboardingViewController = OnboardingContainerViewController()
     let mainViewController = MainViewController()
-     
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
@@ -24,16 +24,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         loginViewController.delegate = self
         onboardingViewController.delegate = self
-
         
+        
+        displayLogin()
+        
+        return true
+    }
+    
+    
+    private func displayLogin(){
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen(){
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        }else{
+            setRootViewController(onboardingViewController)
+        }
+    }
+    
+    private func prepMainView(){
         mainViewController.setStatusBar()
-        
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().backgroundColor = appColor
-        
-        window?.rootViewController = mainViewController
-
-        return true
     }
     
 }
@@ -45,16 +60,13 @@ extension AppDelegate : LoginViewControllerDelegate,OnboardingContainerViewContr
     
     func didFinishingOnboarding() {
         
-        setRootViewController(mainViewController)
         LocalState.hasOnboarded = true
+        prepMainView()
+        setRootViewController(mainViewController)
     }
     
     func didLogin() {
-        if LocalState.hasOnboarded == false {
-            setRootViewController(onboardingViewController)
-        }else{
-            setRootViewController(mainViewController)
-        }
+        displayNextScreen()
     }
 }
 
