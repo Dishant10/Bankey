63c583eee24a030cb60142dd805c6f926f5fc15d
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-11-26 21:56:23 +0530

    Shake the bell

--- Bankey/AccountSummaryHeaderView.swift
+++ Bankey/AccountSummaryHeaderView.swift
@@ -12,6 +12,8 @@ class AccountSummaryHeaderView : UIView {
     
     @IBOutlet var contentView: UIView!
     
+    let shakeyBell = ShakeyBellView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -37,6 +39,20 @@ class AccountSummaryHeaderView : UIView {
         contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        setupShakeyBell()
+    }
+    
+    private func setupShakeyBell(){
+        shakeyBell.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(shakeyBell)
+        
+        NSLayoutConstraint.activate([
+        
+            shakeyBell.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shakeyBell.bottomAnchor.constraint(equalTo: bottomAnchor)
+            
+        ])
     }
     
 }
